b734c158c45ebf17ab8c69cb22648d0e80fe7c4b
Rahul Thube <160202060+rahulthube777@users.noreply.github.com>
2026-04-27 10:08:52 +0530

    Update solution for tanh-activation

--- tanh-activation/tanh-activation.py
+++ tanh-activation/tanh-activation.py
@@ -1,11 +1,13 @@
 import numpy as np
 
-def tanh(x):
-    # return np.divide(np.exp(x) - np.exp(-x), np.exp(x) + np.exp(-x))
-    if( isinstance(x, (int, float))):
-        return (np.exp(x) - np.exp(-x))/ (np.exp(x) + np.exp(-x))
-    list=[]
-    for ele in x:
-        list.append(tanh(ele))
+# def tanh(x):
+#     if( isinstance(x, (int, float))):
+#         return (np.exp(x) - np.exp(-x))/ (np.exp(x) + np.exp(-x))
+#     list=[]
+#     for ele in x:
+#         list.append(tanh(ele))
 
-    return list
+#     return list
+
+def tanh(x):
+    return np.tanh(x)
